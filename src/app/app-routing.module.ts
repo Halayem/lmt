2dc171e028590edbaadaa157b8a983d15229f7ab
Anis Halayem <anis.halayem@gmail.com>
2019-11-06 08:14:59 +0100

    Component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserInformationComponent } from './components/user-information/user-information.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:       'user-information',
+    component:  UserInformationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserInformationComponent } from './components/user-information/user-information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInformationComponent
   ],
   imports: [
     BrowserModule,
