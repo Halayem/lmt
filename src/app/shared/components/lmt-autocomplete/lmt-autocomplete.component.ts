76dab0015b595578b6d30e47ba2b884bef79585a
Anis Halayem <anis.halayem@gmail.com>
2019-11-26 17:55:17 +0100

    Implement on select and store complete object

--- src/app/shared/components/lmt-autocomplete/lmt-autocomplete.component.ts
+++ src/app/shared/components/lmt-autocomplete/lmt-autocomplete.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { LmtAutocompleteParameter } from './model/lmt-autocomplete-param';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { LmtAutocompleteConfigurationModel } from './model/lmt-autocomplete-config';
 import { LMT_AUTO_COMPLETE_DEFAULT_CONFIGURATION } from './config/lmt-autocomplete-configs';
 import { FormControl } from '@angular/forms';
 import { startWith, map } from 'rxjs/operators';
-
+import { SkillService } from 'src/app/user-registry/user-project/service/skill.service';
+import * as R from 'ramda';
+import { MatChipInputEvent, MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material';
 @Component({
   selector:     'app-lmt-autocomplete',
   templateUrl:  './lmt-autocomplete.component.html',
@@ -13,21 +15,51 @@ import { startWith, map } from 'rxjs/operators';
 })
 export class LmtAutocompleteComponent implements OnInit {
 
-  @Input() lmtAutocompleteParam:  LmtAutocompleteParameter;
-  @Input() lmtAutocompleteConfig: LmtAutocompleteConfigurationModel;
+  @ViewChild ( 'auto',      { static: false } ) matAutocomplete:  MatAutocomplete;
+  @ViewChild ( 'itemInput', { static: false })  itemInput:        ElementRef<HTMLInputElement>;
+  // @Input() lmtAutocompleteParam:  LmtAutocompleteParameter;
+  // @Input() lmtAutocompleteConfig: LmtAutocompleteConfigurationModel;
+  public lmtAutocompleteParam:  LmtAutocompleteParameter;
+  public lmtAutocompleteConfig: LmtAutocompleteConfigurationModel
+
+  private _selectedItems: any[] = []; // for @Output
 
   private _filteredItems: Observable<any[]>;
   private _itemControl:   FormControl;
-  constructor() { 
-    if ( this.lmtAutocompleteConfig === null ) {
+  
+  constructor( readonly skillService: SkillService ) { 
+    if ( R.isNil( this.lmtAutocompleteConfig ) ) {
       this.lmtAutocompleteConfig = LMT_AUTO_COMPLETE_DEFAULT_CONFIGURATION;
       console.info ( 'LmtAutocompleteComponent - setting lmt autocomplete default configuration: ', this.lmtAutocompleteConfig );
     }
+
+    this.lmtAutocompleteParam = {
+      datasource:     this.skillService.getSkills(),
+      attributeName:  'name'
+    };
+
+    console.log ( 'LmtAutocompleteComponent - ws executed ?');
 
     this._itemControl   = new FormControl();
     this.getFilterCallback().subscribe( filterCallback => this._filteredItems = filterCallback );
   }
 
+  public addElement( matchipInputEvent: MatChipInputEvent ): void {
+    console.log ( 'received event, input: ', matchipInputEvent.input, ' value: ', matchipInputEvent.value );
+    if ( this.matAutocomplete.isOpen ) {
+      return;
+    }
+  }
+
+  public selectedItem( matAutocompleteSelectedEvent: MatAutocompleteSelectedEvent ): void {
+    console.log ( 'selectedItem - received event, input: ', matAutocompleteSelectedEvent.option );
+    
+    this._selectedItems.push  ( matAutocompleteSelectedEvent.option.value );
+    this.itemInput.nativeElement.value = '';
+    this._itemControl.setValue( null );
+
+    console.log( 'selected items', this._selectedItems );
+  }
 
   private getFilterCallback(): Observable<Observable<any[]>> {
     return  ( this._itemControl.valueChanges.pipe(
@@ -39,15 +71,23 @@ export class LmtAutocompleteComponent implements OnInit {
             ) );
   }
 
-  private defaultFilter( searchedItem: string ): Observable<any[]> {
+  private defaultFilter( searchedItem: string | any ): Observable<any[]> {
+    if ( typeof searchedItem !== 'string' ) {
+      return;
+    }
     return this.lmtAutocompleteParam.datasource.pipe(
       map ( items => items.filter ( 
-        item => item[ this.lmtAutocompleteParam.attributeName ].toLowerCase().indexOf( searchedItem.toLowerCase() ) === 0 ) )
+        item => item[ this.lmtAutocompleteParam.attributeName ].toLowerCase()
+                                                               .indexOf( 
+                                                                  searchedItem.toLowerCase() 
+                                                                ) === 0 ) )
     );
   }
 
   ngOnInit() {
   }
 
-  get itemControl() { return this._itemControl; }
+  get itemControl   () { return this._itemControl;    }
+  get filteredItems () { return this._filteredItems;  }
+  get selectedItems () { return this._selectedItems;  }
 }
