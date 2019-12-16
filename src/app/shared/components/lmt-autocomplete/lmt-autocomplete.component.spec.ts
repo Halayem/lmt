bbdc32160614ff3fcbf06fce9a21b1c350ed2979
-add <andryjohns+9@gmail.com>
2019-12-16 14:01:24 +0100

    test lmtautocompltecomponen

--- src/app/shared/components/lmt-autocomplete/lmt-autocomplete.component.spec.ts
+++ src/app/shared/components/lmt-autocomplete/lmt-autocomplete.component.spec.ts
@@ -3,6 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LmtAutocompleteComponent } from './lmt-autocomplete.component';
 import { MaterialModule } from 'src/app/material/material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LmtAutocompleteComponent', () => {
   let component: LmtAutocompleteComponent;
@@ -13,6 +15,7 @@ describe('LmtAutocompleteComponent', () => {
       imports: [        
         MaterialModule,
         ReactiveFormsModule,
+        BrowserAnimationsModule,
         FormsModule
       ],
       declarations: [ LmtAutocompleteComponent ]
@@ -29,4 +32,63 @@ describe('LmtAutocompleteComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('the component is not ready', done => {
+    let comp = new LmtAutocompleteComponent();
+    comp.componentReady.subscribe(ready => {
+      expect(ready).toBe(false);
+      done();
+    })
+  });
+
+  
+  it('the component is ready', done => {
+    let comp = new LmtAutocompleteComponent();
+    comp.lmtAutocompleteParam =  {
+      datasource:             [],
+      attributeNameToDisplay: 'name',
+      attributeNameForFilter: 'name',
+      attributeNameKey:       'id',
+    };
+    comp.componentReady.subscribe(ready => {
+      expect(ready).toBe(true);
+      done();
+    })
+  });
+
+  it('verify length of selecteItems', () => {
+    let comp = new LmtAutocompleteComponent();
+    comp.lmtAutocompleteParam =  {
+      datasource:             [ {
+        "id": 5,
+        "name": "big data senior"
+
+        },
+        {
+            "id": 6,
+            "name": "big data confirmé"
+
+        }
+      ],
+      attributeNameToDisplay: 'name',
+      attributeNameForFilter: 'name',
+      attributeNameKey:       'id',
+    };
+    comp.componentReady.next(true);
+    fixture.detectChanges();
+    const trigger = fixture.debugElement.query(By.css('.test'))
+  
+    
+    console.log('trigger', trigger);
+    console.log('trigger2', fixture.debugElement.query(By.css('.test')));
+    trigger.nativeElement.click();
+
+    expect(comp.selectedItems.length).toEqual(3)
+    comp.selectedItem;
+    expect(comp.selectedItems.length).toEqual(2)
+    comp.removeItem;
+    expect(comp.selectedItems.length).toEqual(1)
+
+  });
+
 });
